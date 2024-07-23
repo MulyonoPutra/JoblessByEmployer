bd861481c12f58dcadc11a6a578eec6bfc8086bc
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-07-23 09:42:09 +0700

    several changes in Navbar Component.

--- src/app/shared/components/molecules/navbar/navbar.component.ts
+++ src/app/shared/components/molecules/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    AngularSvgIconModule
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
