18aed8b7142a08aa9889ee2975c70f730597e9e3
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-07-24 12:04:53 +0700

    feat: login integration, icons and etc.
    - add several icons
    - login integration
    - changes class name
    - remove unused code

---
+++ src/app/core/constants/icons.ts
@@ -0,0 +1,7 @@
+export const SvgIcons = {
+    checklistGreen: '../../../../../assets/icons/checklist-green.svg',
+    closeRed: '../../../../../assets/icons/close-red.svg',
+    eyeOpen: '../../../../../assets/icons/eye-open.svg',
+    eyeClose: '../../../../../assets/icons/eye-close.svg',
+    arrowLeft: '../../../../../assets/icons/arrow-left.svg',
+};
