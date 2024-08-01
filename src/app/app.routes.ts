08ebe03b33232178449c9a2df72467274b453e28
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-08-01 11:04:02 +0700

    feat: add authorization role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { PageNotFoundComponent } from './core/layout/page-not-found/page-not-found.component';
 import { Routes } from '@angular/router';
+import { authenticationGuard } from './core/guards/authentication.guard';
+import { authorizationGuard } from './core/guards/authorization.guard';
 
 export const routes: Routes = [
     {
@@ -13,15 +15,21 @@ export const routes: Routes = [
                     import('./features/home/pages/home/home.component').then(
                         (c) => c.HomeComponent,
                     ),
+                // canActivate: [authorizationGuard],
+                // data: {role: 'employer' },
             },
             {
                 path: 'account',
                 loadChildren: () =>
                     import('./features/account/pages/acount.routes').then((c) => c.ACCOUNT_ROUTES),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
             },
             {
                 path: 'jobs',
                 loadChildren: () => import('./features/jobs/job.routes').then((c) => c.JOB_ROUTES),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
             },
             {
                 path: 'contact',
@@ -29,8 +37,11 @@ export const routes: Routes = [
                     import('./features/contact/contact-us/contact-us.component').then(
                         (c) => c.ContactUsComponent,
                     ),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
             },
         ],
+    canActivate: [authenticationGuard],
     },
     {
         path: 'auth',
--- src/app/core/guards/authorization.guard.ts
+++ src/app/core/guards/authorization.guard.ts
@@ -1,18 +1,25 @@
 import { inject } from '@angular/core';
 import { Router, type CanActivateFn } from '@angular/router';
 import { StorageService } from '../../shared/services/storage.service';
+import { ToastService } from '../../shared/services/toast.service';
 
 export const authorizationGuard: CanActivateFn = (route) => {
     const storageService: StorageService = inject(StorageService);
+    const toastService: ToastService = inject(ToastService);
     const router: Router = inject(Router);
     const token = storageService.getAccessToken();
     const role = storageService.getRole();
 
     if (token) {
         if (route.data['role'] && route.data['role'].indexOf(role) === -1) {
+            toastService.showWarnToast('You are not allowed!', 'Login or Register using Employer account to access this page.');
             router.navigate(['/']);
+
             return false;
         }
+    } else {
+      router.navigate(['/login']);
+      return false;
     }
     return true;
 };
