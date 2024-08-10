53e1cff0f464160c1e9edc4028315e2973edce33
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-08-10 11:13:08 +0700

    style: format code using prettier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,45 +4,45 @@ import { authorizationGuard } from './core/guards/authorization.guard';
 import { PageNotFoundComponent } from './core/layout/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./core/layout/main/main.component').then((c) => c.MainComponent),
-    children: [
-      {
+    {
         path: '',
-        loadChildren: () =>
-          import('./features/home/home.routes').then((c) => c.HOME_ROUTES),
-      },
-      {
-        path: 'account',
-        loadChildren: () =>
-          import('./features/account/pages/acount.routes').then((c) => c.ACCOUNT_ROUTES),
-        canActivate: [authorizationGuard],
-        data: { role: 'employer' },
-      },
-      {
-        path: 'jobs',
-        loadChildren: () => import('./features/jobs/job.routes').then((c) => c.JOB_ROUTES),
-        canActivate: [authorizationGuard],
-        data: { role: 'employer' },
-      },
-      {
-        path: 'contact',
         loadComponent: () =>
-          import('./features/contact/contact-us/contact-us.component').then(
-            (c) => c.ContactUsComponent,
-          ),
-        canActivate: [authorizationGuard],
-        data: { role: 'employer' },
-      },
-    ],
-    canActivate: [authenticationGuard],
-  },
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./features/authentication/authentication.routes').then((c) => c.AUTH_ROUTES),
-  },
-  { path: '**', component: PageNotFoundComponent },
+            import('./core/layout/main/main.component').then((c) => c.MainComponent),
+        children: [
+            {
+                path: '',
+                loadChildren: () =>
+                    import('./features/home/home.routes').then((c) => c.HOME_ROUTES),
+            },
+            {
+                path: 'account',
+                loadChildren: () =>
+                    import('./features/account/pages/acount.routes').then((c) => c.ACCOUNT_ROUTES),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
+            },
+            {
+                path: 'jobs',
+                loadChildren: () => import('./features/jobs/job.routes').then((c) => c.JOB_ROUTES),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
+            },
+            {
+                path: 'contact',
+                loadComponent: () =>
+                    import('./features/contact/contact-us/contact-us.component').then(
+                        (c) => c.ContactUsComponent,
+                    ),
+                canActivate: [authorizationGuard],
+                data: { role: 'employer' },
+            },
+        ],
+        canActivate: [authenticationGuard],
+    },
+    {
+        path: 'auth',
+        loadChildren: () =>
+            import('./features/authentication/authentication.routes').then((c) => c.AUTH_ROUTES),
+    },
+    { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/features/home/pages/about/about.component.ts
+++ src/app/features/home/pages/about/about.component.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [
-    CommonModule,
-    AngularSvgIconModule
-  ],
-  templateUrl: './about.component.html',
-  styleUrls: [ './about.component.scss' ],
+    selector: 'app-about',
+    standalone: true,
+    imports: [CommonModule, AngularSvgIconModule],
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent { }
+export class AboutComponent {}
