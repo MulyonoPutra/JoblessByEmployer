96bc7523ddfd1b8e256ab15cf546382659b475d2
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-08-10 11:11:52 +0700

    fix: change using svg-icon

--- src/app/shared/components/molecules/footer/footer.component.ts
+++ src/app/shared/components/molecules/footer/footer.component.ts
@@ -1,11 +1,13 @@
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../atoms/logo/logo.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [CommonModule, LogoComponent],
+    imports: [CommonModule, RouterModule, AngularSvgIconModule, LogoComponent],
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
 })
